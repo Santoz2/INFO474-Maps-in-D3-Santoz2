413427cc6a29438bec8e6dcde67d5afbb5d40b4f
Zaijen Santos <santoz2@uw.edu>
2020-02-26 17:13:33 -0800

    Add fade transition

--- index.js
+++ index.js
@@ -64,6 +64,18 @@ d3.json('./nygeo.json').then(function(data) {
                 })
                 .attr('r', 5)
                 .attr('fill', '#4682b4')
+                .on( "click", function(){
+                    d3.select(this)
+                      .attr("opacity",1)
+                      .transition()
+                      .duration( 1000 )
+                      .attr( "cx", m.width * Math.round( Math.random() ) )
+                      .attr( "cy", m.height * Math.round( Math.random() ) )
+                      .attr( "opacity", 0 )
+                      .on("end",function(){
+                        d3.select(this).remove();
+                      })
+                  })
 
         
     })
